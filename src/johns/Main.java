ab3ebcc2c0a3719443edd54590e77c7134a757b4
jeremiahorcutt <jeremiahorcutt@gmail.com>
2021-05-18 13:51:26 -0600

    Team member salutation (JO)

--- src/johns/Main.java
+++ src/johns/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello, my name is Emma!");
+        System.out.println("Hello Emma, my name is Jeremiah!");
     }
 }
